2468c5371a3336a1acc55489b176bbe0dd86bc99
Lukas Wanko <lwanko.mmt-b2010@fh-salzburg.ac.at>
2012-01-12 11:09:00 +0100

    first try

---
+++ trinkwasserbrunnen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  trinkwasserbrunnen
+//
+//  Created by Mahmood1 on 1/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
